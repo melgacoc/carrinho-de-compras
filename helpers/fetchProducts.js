3bed8b73905fc591e3027c91f15f0caea0e52864
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-13 17:25:57 -0300

    req 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,18 @@
-const fetchProducts = () => {
+const fetchProducts = async (product) => {
   // seu código aqui
+  const base = 'https://api.mercadolibre.com/sites/MLB/search?q='
+  const url = `${base}${product}`;
+
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    return error;
+  }
 };
+
+fetchProducts(computador);
 
 if (typeof module !== 'undefined') {
   module.exports = {
