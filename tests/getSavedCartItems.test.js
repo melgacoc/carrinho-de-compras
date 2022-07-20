757d2b3c002488f9a40f581fa4116c629810b51a
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-20 19:22:46 -0300

    req 6 e 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('testa se ao executar a função com parametro a função é chamada', () => {
+    getSavedCartItems(1)
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+  it('testa se ao executar a função com o argumento é chamado 2 parametros', () => {
+    getSavedCartItems(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems');
+  });
+  it('testa se a função retorna erro quando não houver argumento', () => {
+    expect(getSavedCartItems(undefined)).toThrow(Error);
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('testa se ao executar a função com parametro a função é chamada', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+  it('testa se ao executar a função com o argumento é chamado 2 parametros', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>')
+  });
+  it('testa se a função retorna erro quando não houver argumento', () => {
+    expect(saveCartItems(undefined)).toThrow(Error);
+  });
 });
